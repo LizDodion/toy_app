f95430608e5dc722072aa5b92e1a35bbe47a13b7
Elizabeth Phillips <Elizabeth.Phillips.Contractor@dvla.gov.uk>
2019-06-21 19:52:30 +0100

    postgres in prod

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+  validates :email, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, uniqueness:  { case_sensitive: false }
   validates :name, presence: true, length: { minimum: 5 }, format: /(\w|\.|\'| )+\z/
 
   # validates :name, presence: true, length: { minimum: 5 }, format: /\A[^0-9`!@#\$%\^&*+_=;\[\]<>]+\z/
