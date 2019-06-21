0e29c0ec008cc03d4e20032d7c8b35ea8664c696
Elizabeth Phillips <Elizabeth.Phillips.Contractor@dvla.gov.uk>
2019-06-21 19:40:11 +0100

    validate name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :email, presence: true, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
+  validates :name, presence: true, length: { minimum: 5 }, format: /(\w|\.|\'| )+\z/
+
+  # validates :name, presence: true, length: { minimum: 5 }, format: /\A[^0-9`!@#\$%\^&*+_=;\[\]<>]+\z/
 end
